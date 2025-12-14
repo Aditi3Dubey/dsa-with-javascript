d0e39991b1ad4273876778369d969863b215e08f
Adit3 Dubey <daditi280@gmail.com>
2025-12-14 22:05:35 +0530

    feat: add inverted triangle star pattern

---
+++ Pattern programs/inverted_triangle.js
@@ -0,0 +1,20 @@
+/*
+
+           * * * * *
+           * * * *
+           * * *
+           * *
+           * 
+
+
+ */
+
+// Rows :5
+
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 5; j >= i; j--) {
+    row += "* ";
+  }
+  console.log(row);
+}
